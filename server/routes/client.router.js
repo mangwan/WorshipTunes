f6ad224f7da87b7fac4a32203dbac592a7805b9e
Mang Wan <vang92@gmail.com>
2019-07-02 13:51:50 -0500

    fixed 404 error on requst song page

--- server/routes/client.router.js
+++ server/routes/client.router.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+
+
+// Handles Ajax request for user information if user is authenticated
+router.get('/', rejectUnauthenticated, (req, res) => {
+    // Send back user object from the session (previously queried from the database)
+    res.send(req.user);
+  });
 
 //grab songs to post on home page
 router.get('/song', async (req, res) => {
